16fef152c0fce4a3b24df3ea7a7a3dfbc9709e39
miguelsndc <mglsndc@gmail.com>
2022-07-28 21:48:01 -0300

    fix tailwindcss not working; add smaller breakpoint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {},
+    screens: {
+      xs: '512px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
